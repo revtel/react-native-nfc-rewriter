8818c404a4f09e56c48087f8b436a705cb27ad52
Richie <whitedogg13@gmail.com>
2020-02-19 21:31:04 +0800

    add some ui stuff

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,25 +1,45 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import { Button } from 'react-native-elements';
 import NfcProxy from '../NfcProxy';
 
 function HomeScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button 
-        title="Scan Tag" 
-        onPress={
-          async () => {
+    <View style={{flex: 1, padding: 20}}>
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+            alignSelf: 'center',
+          }}
+          source={require('../../images/nfc-512.png')}
+        />
+      </View>
+
+      <View style={{flex: 1}}>
+        <Button
+          title="Scan Tag"
+          style={{alignSelf: 'center', width: 240}}
+          onPress={async () => {
             try {
               const tag = await NfcProxy.readTag();
-              navigation.navigate('TagDetail', { tag });
+              navigation.navigate('TagDetail', {tag});
             } catch (ex) {
               console.warn('readTag ex', ex);
             }
             await NfcProxy.abort();
-          }
-        }
-      />
+          }}
+        />
+
+        <Button
+          title="Write Tag"
+          style={{alignSelf: 'center', width: 240, marginTop: 15}}
+          onPress={async () => {
+          }}
+        />
+      </View>
+
     </View>
   );
 }
--- src/Screens/TagDetailScreen.js
+++ src/Screens/TagDetailScreen.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import {
-  View, Text
+  View, Text, Clipboard, Alert, TouchableOpacity, ScrollView,
 } from 'react-native';
+import TagItem from '../Components/TagItem'
 
 class TagDetailScreen extends React.Component {
   render() {
     const { tag } = this.props.route.params;
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 10 }}>
-        <Text>{JSON.stringify(tag, null, 2)}</Text>
+      <View style={{ flex: 1, paddingTop: 10, alignItems: 'center'}}>
+        <TagItem card={tag} />
+
+        <ScrollView style={{marginTop: 10, flex: 1, alignSelf: 'stretch', padding: 10}}>
+          <Text style={{fontSize: 20}}>Tag Detail</Text>
+          <Text style={{fontSize: 12, color: 'grey'}}>* long press to copy</Text>
+          <TouchableOpacity 
+            style={{marginTop: 10}}
+            onLongPress={this._copyTagInfo}
+          >
+            <Text>{JSON.stringify(tag, null, 2)}</Text>
+          </TouchableOpacity>
+        </ScrollView>
+
       </View>
     )
+  }
+
+  _copyTagInfo = () => {
+    const { tag } = this.props.route.params;
+    Clipboard.setString(JSON.stringify(tag, null, 2));
+    Alert.alert('Copied to clipboard!');
   }
 }
 
