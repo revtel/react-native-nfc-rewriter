572ce3b8e203033732e80dcd42261c941b4f6aa6
Richie <whitedogg13@gmail.com>
2020-11-28 09:11:29 +0800

    feat: add LandingScreen

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,38 +1,60 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import LandingScreen from './Screens/LandingScreen';
 import HomeScreen from './Screens/HomeScreen';
 import TagDetailScreen from './Screens/TagDetailScreen';
 import NdefWriteScreen from './Screens/NdefWriteScreen';
 import CustomPayloadScreen from './Screens/CustomPayloadScreen';
 
-const Stack = createStackNavigator();
+const MainStack = createStackNavigator();
+
+function Main(props) {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{title: 'Home'}}
+      />
+      <MainStack.Screen
+        name="TagDetail"
+        component={TagDetailScreen}
+        options={{title: 'Tag Info'}}
+      />
+      <MainStack.Screen
+        name="NdefWrite"
+        component={NdefWriteScreen}
+        options={{title: 'NDEF Write'}}
+      />
+      <MainStack.Screen
+        name="CustomPayload"
+        component={CustomPayloadScreen}
+        options={{title: 'Custom Payload'}}
+      />
+    </MainStack.Navigator>
+  )
+}
+
+const RootStack = createStackNavigator();
+
+function Root(props) {
+  return (
+    <RootStack.Navigator headerMode="none" mode="modal">
+      <RootStack.Screen name="Landing" component={LandingScreen} />
+      <RootStack.Screen
+        name="Main"
+        component={Main}
+        options={{animationEnabled: false}}
+      />
+    </RootStack.Navigator>
+  );
+}
 
 function AppNavigator(props) {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Home'}}
-        />
-        <Stack.Screen
-          name="TagDetail"
-          component={TagDetailScreen}
-          options={{title: 'Tag Info'}}
-        />
-        <Stack.Screen
-          name="NdefWrite"
-          component={NdefWriteScreen}
-          options={{title: 'NDEF Write'}}
-        />
-        <Stack.Screen
-          name="CustomPayload"
-          component={CustomPayloadScreen}
-          options={{title: 'Custom Payload'}}
-        />
-      </Stack.Navigator>
+      <Root />
     </NavigationContainer>
   );
 }
---
+++ src/Screens/LandingScreen.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  View,
+  ImageBackground,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function LandingScreen(props) {
+  React.useEffect(() => {
+    async function initialize() {
+      await delay(1000);
+
+      props.navigation.reset({
+        index: 0,
+        routes: [{name: 'Main'}],
+      });
+    }
+
+    initialize();
+  }, [props.navigation]);
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../../images/nfc-512.png')}
+        resizeMode="contain"
+        style={styles.image}></ImageBackground>
+
+      <ActivityIndicator size='large' style={{marginTop: 50}} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 250,
+    height: 250,
+    backgroundColor: 'transparent',
+  },
+});
+
+export default LandingScreen;
