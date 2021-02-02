93c20d1db5833772aa93c917735702e526ddba7c
Richie <whitedogg13@gmail.com>
2021-02-02 09:46:13 +0800

    feat: use new NfcTech's getter interface

--- src/NfcProxy.js
+++ src/NfcProxy.js
@@ -3,7 +3,6 @@ import NfcManager, {NfcTech, Ndef, NfcEvents} from 'react-native-nfc-manager';
 import {AppEvents, AppEventName} from './AppEvents';
 
 const NfcAndroidUI = AppEvents.get(AppEventName.NFC_SCAN_UI);
-const LogRecord = AppEvents.get(AppEventName.LOG_RECORD);
 
 class ErrSuccess extends Error {}
 
@@ -42,21 +41,14 @@ class NfcProxy {
   readTag = async () => {
     let tag = null;
     try {
-      if (Platform.OS === 'ios') {
-        // because we also want to read the id from it!
-        await NfcManager.requestTechnology([
-          NfcTech.MifareIOS,
-          NfcTech.Iso15693IOS,
-          NfcTech.IsoDep,
-          NfcTech.Ndef,
-        ]);
-      } else {
+      if (Platform.OS === 'android') {
         NfcAndroidUI.emit('OPEN');
-        await NfcManager.requestTechnology([NfcTech.Ndef]);
       }
 
+      await NfcManager.requestTechnology([NfcTech.Ndef]);
+
       tag = await NfcManager.getTag();
-      tag.ndefStatus = await NfcManager.getNdefHandler().getNdefStatus();
+      tag.ndefStatus = await NfcManager.ndefHandler.getNdefStatus();
     } catch (ex) {
       console.warn(ex);
     }
@@ -86,7 +78,7 @@ class NfcProxy {
       }
 
       if (bytes) {
-        await NfcManager.getNdefHandler().writeNdefMessage(bytes);
+        await NfcManager.ndefHandler.writeNdefMessage(bytes);
 
         if (Platform.OS === 'ios') {
           await NfcManager.setAlertMessageIOS('Successfully write NDEF');
@@ -107,20 +99,14 @@ class NfcProxy {
     let responses = [];
 
     try {
-      if (Platform.OS === 'ios') {
-        await NfcManager.requestTechnology([NfcTech.MifareIOS]);
-      } else {
+      if (Platform.OS == 'android') {
         NfcAndroidUI.emit('OPEN');
-        await NfcManager.requestTechnology([NfcTech.Ndef]);
       }
 
+      await NfcManager.requestTechnology([NfcTech.NfcA]);
+
       for (const payload of payloads) {
-        let resp;
-        if (Platform.OS === 'ios') {
-          resp = await NfcManager.sendMifareCommandIOS(payload);
-        } else {
-          resp = await NfcManager.transceive(payloads);
-        }
+        const resp = await NfcManager.nfcAHandler.transceive(payloads);
         responses.push(resp);
         await delay(100);
       }
@@ -142,42 +128,6 @@ class NfcProxy {
     NfcManager.cancelTechnologyRequest().catch(() => 0);
     NfcAndroidUI.emit('CLOSE');
   };
-
-  transceive = async (bytes, msg) => {
-    this.dbgLog(msg, 'send', bytes);
-    let respBytes = null;
-    if (Platform.OS === 'ios') {
-      respBytes = await NfcManager.sendMifareCommandIOS(bytes);
-    } else {
-      respBytes = await NfcManager.transceive(bytes);
-    }
-    this.dbgLog(msg, 'recv', respBytes);
-    return respBytes;
-  };
-
-  dbgLog = (msg, type, data) => {
-    let dataStr = '';
-
-    if (Array.isArray(data)) {
-      for (let byte of data) {
-        dataStr += toHex(byte) + ' ';
-      }
-    } else if (data !== undefined) {
-      dataStr = toHex(data);
-    }
-
-    console.log(`[${msg}-${type}] ${dataStr}`);
-    LogRecord.emit({message: msg, type, data: dataStr, created: new Date()});
-  };
-
-  calcChecksum = (bytes) => {
-    let result = 0;
-    for (let byte of bytes) {
-      result = result ^ byte;
-      result &= 0xff;
-    }
-    return result;
-  };
 }
 
 // ------------------------
@@ -185,48 +135,6 @@ class NfcProxy {
 // ------------------------
 const delay = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-const toHex = (num) => {
-  return ('00' + num.toString(16)).slice(-2);
-};
-
-const arrToHex = (arr) => {
-  let hex = '';
-  for (let byte of arr) {
-    hex += toHex(byte);
-  }
-  return hex;
-};
-
-const uidToBytes = (uidStr) => {
-  if (uidStr.length !== 14) {
-    throw new Error('incorrect uid length');
-  }
-
-  let bytes = [];
-  for (let i = 0; i < 14; i = i + 2) {
-    bytes.push(parseInt(uidStr.slice(i, i + 2), 16));
-  }
-  return bytes;
-};
-
-const pinToBytes = (pinStr) => {
-  pinStr = ('000000' + parseInt(pinStr).toString(16)).slice(-6);
-
-  let bytes = [];
-  for (let i = 0; i < pinStr.length; i = i + 2) {
-    bytes.push(parseInt(pinStr.slice(i, i + 2), 16));
-  }
-  return bytes;
-};
-
-const hexToBytes = (hex) => {
-  let bytes = [];
-  for (let i = 0; i < hex.length; i = i + 2) {
-    bytes.push(parseInt(hex.slice(i, i + 2), 16));
-  }
-  return bytes;
 };
 
 export default new NfcProxy();
