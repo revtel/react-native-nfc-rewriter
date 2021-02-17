5695865f0355056b6da67228674d2cbbf4d64d50
Richie <whitedogg13@gmail.com>
2021-02-17 22:11:24 +0800

    feat: update landing screen

--- src/Screens/LandingScreen.js
+++ src/Screens/LandingScreen.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import {
-  View,
-  ImageBackground,
-  StyleSheet,
-  ActivityIndicator,
-} from 'react-native';
+import {View, StyleSheet, ActivityIndicator, Animated} from 'react-native';
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function LandingScreen(props) {
+  const opacityAnimValue = React.useRef(new Animated.Value(0)).current;
+  const scaleAnimValue = React.useRef(new Animated.Value(1)).current;
+
   React.useEffect(() => {
     async function initialize() {
+      Animated.timing(opacityAnimValue, {
+        duration: 1000,
+        toValue: 1,
+        useNativeDriver: true,
+      }).start();
+
       await delay(1000);
+
+      Animated.parallel([
+        Animated.timing(opacityAnimValue, {
+          duration: 800,
+          toValue: 0.2,
+          useNativeDriver: true,
+        }),
+        Animated.timing(scaleAnimValue, {
+          duration: 800,
+          toValue: 4,
+          useNativeDriver: true,
+        }),
+      ]).start();
+
+      await delay(600);
 
       props.navigation.reset({
         index: 0,
@@ -20,14 +39,17 @@ function LandingScreen(props) {
     }
 
     initialize();
-  }, [props.navigation]);
+  }, [props.navigation, opacityAnimValue, scaleAnimValue]);
 
   return (
     <View style={styles.container}>
-      <ImageBackground
+      <Animated.Image
         source={require('../../images/nfc-512.png')}
         resizeMode="contain"
-        style={styles.image}
+        style={[
+          styles.image,
+          {opacity: opacityAnimValue, transform: [{scale: scaleAnimValue}]},
+        ]}
       />
 
       <ActivityIndicator size="large" style={{marginTop: 50}} />
