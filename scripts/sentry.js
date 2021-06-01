7264743b9a77c1c15042ee3a58a0e8c2a4bc9a3f
Richie <whitedogg13@gmail.com>
2021-06-01 10:28:38 +0800

    feat: more on sentry integration

--- scripts/sentry.js
+++ scripts/sentry.js
@@ -11,47 +11,52 @@ require('dotenv').config();
 
 async function main() {
   const platform = process.env.REV_PLATFORM;
+
+  if (['ios', 'android'].indexOf(platform) === -1) {
+    throw new Error('platform should be either ios or android');
+  }
+
   const spinner = ora('processing...');
   const releaseName = `${revAppId}@${version}`;
+  const bundleName =
+    platform === 'ios' ? 'main.jsbundle' : 'index.android.bundle';
   let resp;
 
-  console.log(`[${releaseName}]\n`);
+  console.log(`App [${releaseName}] for ${platform}\n`);
 
-  if (platform === 'ios') {
-    console.log('[command req] bundling ios');
-    try {
-      spinner.start();
-      resp = await execa.command(
-        'react-native bundle \
+  try {
+    console.log('[command req] generating RN bundle and sourcemap...');
+    spinner.start();
+    resp = await execa.command(
+      `react-native bundle \
         --dev false \
-        --platform ios \
+        --platform ${platform} \
         --entry-file index.js \
-        --bundle-output main.jsbundle \
-        --sourcemap-output main.jsbundle.map',
-      );
-      console.log('\n[command resp]\n' + resp.stdout);
+        --bundle-output ${bundleName} \
+        --sourcemap-output ${bundleName}.map`,
+    );
+    spinner.stop();
+    console.log('\n[command resp]\n' + resp.stdout);
 
-      console.log('[command req] upload sourcemap');
-      spinner.start();
-      // TODO: not sure how to pass --dist
-      resp = await execa.command(
-        `node_modules/@sentry/cli/bin/sentry-cli --auth-token ${
-          process.env.SENTRY_TOKEN
-        } releases -o ${process.env.SENTRY_ORG} -p ${
-          process.env.SENTRY_PROJECT
-        }\
+    console.log('[command req] uploading sourcemap...');
+    spinner.start();
+    // TODO: not sure how to pass --dist
+    resp = await execa.command(
+      `node_modules/@sentry/cli/bin/sentry-cli --auth-token ${
+        process.env.SENTRY_TOKEN
+      } releases -o ${process.env.SENTRY_ORG} -p ${process.env.SENTRY_PROJECT}\
         files ${releaseName} \
         upload-sourcemaps \
         --dist ${version} \
         --strip-prefix ${process.cwd()}\
-        --rewrite main.jsbundle main.jsbundle.map`,
-      );
-      console.log('\n[command resp]\n' + resp.stdout);
-    } catch (ex) {
-      console.warn(ex);
-    } finally {
-      spinner.stop();
-    }
+        --rewrite ${bundleName} ${bundleName}.map`,
+    );
+    spinner.stop();
+    console.log('\n[command resp]\n' + resp.stdout);
+  } catch (ex) {
+    console.warn(ex);
+  } finally {
+    spinner.stop();
   }
 }
 
