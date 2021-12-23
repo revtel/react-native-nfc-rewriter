86c9e8c2c4d38b7be04b09367c9af7ae690f61b5
richie <whitedogg13@gmail.com>
2021-12-23 15:15:06 +0800

    feat: verify ntag424 signature

--- src/data/ntag-424.js
+++ src/data/ntag-424.js
@@ -1,3 +1,6 @@
+import {Alert} from 'react-native';
+import NfcProxy from '../NfcProxy';
+
 const readSignature = {
   name: 'nxp424-read-sig',
   payload: {
@@ -9,6 +12,33 @@ const readSignature = {
       },
       {type: 'command', payload: [144, 60, 0, 0, 1, 0, 0]},
     ],
+  },
+  onPostExecute: async (result) => {
+    const [success, resps] = result;
+    if (success) {
+      try {
+        const tag = await NfcProxy.readTag();
+        const sig = resps[1]
+          .reduce((acc, byte) => {
+            return (
+              acc + ('00' + byte.toString(16).toUpperCase()).slice(-2) + ' '
+            );
+          }, '')
+          .slice(0, -4);
+        console.warn('ntag-424.onPostExecute sig', sig);
+        console.warn('ntag-424.onPostExecute uid', tag.id);
+        const endpoint = `https://2j6p8l98z5.execute-api.ap-northeast-1.amazonaws.com/prod/nxp21x/verify?uid=${tag.id}&sig=${sig}`;
+        const resp = await (await fetch(endpoint)).json();
+        console.warn('verify resp', resp);
+        if (resp.result) {
+          Alert.alert('Signature is correct');
+        } else {
+          Alert.alert('Signature is wrong');
+        }
+      } catch (ex) {
+        console.warn(ex);
+      }
+    }
   },
 };
 
