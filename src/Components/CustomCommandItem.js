d9f7cdf9de980bcaccfa1250290c30143c330009
Richie <whitedogg13@gmail.com>
2022-06-25 21:26:25 +0800

    feat: align 8 bytes per command row

--- src/Components/CustomCommandItem.js
+++ src/Components/CustomCommandItem.js
@@ -3,6 +3,33 @@ import {View, Text} from 'react-native';
 import {IconButton} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+function PrettyCommandPayloadBytes(props) {
+  const {bytes: _bytes} = props;
+
+  const rows = [];
+  let bytes = [..._bytes];
+  while (bytes.length > 0) {
+    rows.push(bytes.slice(0, 8));
+    bytes = bytes.slice(8);
+  }
+
+  return (
+    <View>
+      {rows.map((row, idx) => {
+        return (
+          <Text key={idx}>
+            {row.reduce((acc, byte) => {
+              return (
+                acc + ('00' + byte.toString(16).toUpperCase()).slice(-2) + ' '
+              );
+            }, '')}
+          </Text>
+        );
+      })}
+    </View>
+  );
+}
+
 function CommandItem(props) {
   const {cmd, resp, onDelete, onEdit, readOnly, onMoveUp, onMoveDown} = props;
   const wrapperStyle = {
@@ -18,23 +45,11 @@ function CommandItem(props) {
     innerElem = (
       <View style={{flex: 1}}>
         <Text style={{marginRight: 5, color: 'gray'}}>TRANSCEIVE</Text>
-        <Text>
-          {cmd.payload.reduce((acc, byte) => {
-            return (
-              acc + ('00' + byte.toString(16).toUpperCase()).slice(-2) + ' '
-            );
-          }, '')}
-        </Text>
+        <PrettyCommandPayloadBytes bytes={cmd.payload} />
         {Array.isArray(resp) && (
           <>
             <Text style={{marginRight: 5, color: 'gray'}}>RESPONSE</Text>
-            <Text>
-              {resp.reduce((acc, byte) => {
-                return (
-                  acc + ('00' + byte.toString(16).toUpperCase()).slice(-2) + ' '
-                );
-              }, '')}
-            </Text>
+            <PrettyCommandPayloadBytes bytes={resp} />
           </>
         )}
       </View>
