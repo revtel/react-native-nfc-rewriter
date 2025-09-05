b6a9142d7829e2d72c9398639b7d12bf4847f67d
Richie <whitedogg13@gmail.com>
2025-09-05 12:02:40 +0800

    feat: rn 0.77.3 upgrade

--- ios/NfcOpenReader/AppDelegate.mm
+++ ios/NfcOpenReader/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import <React/RCTBundleURLProvider.h>
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
 #import <React/RCTLinkingManager.h>
 #import "NfcManager.h"
 
@@ -26,6 +27,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.moduleName = @"NfcOpenReader";
+    self.dependencyProvider = [RCTAppDependencyProvider new];
     // You can add your custom initial props in the dictionary below.
     // They will be passed down to the ViewController used by React Native.
     self.initialProps = @{};
